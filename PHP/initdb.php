333c449694c4522708da5f2f08f1dadb6d1580d7
Anders Ardo <Anders.Ardo@gmail.com>
2015-05-21 20:13:33 +0200

    Changed MySQL database name to RGDindatavalid

--- PHP/initdb.php
+++ PHP/initdb.php
@@ -5,16 +5,16 @@ $db_server = "localhost";
 $db_user = "rgd";
 //$db_password = "";
 $db_password = "pilot";
-$db="test";
+$db="RGDindatavalid";
 $link = mysql_connect($db_server, $db_user, $db_password);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully to '.$link."<br/>";
-// make test the current db
-$db_selected = mysql_select_db('test', $link);
+// make RGDindatavalid the current db
+$db_selected = mysql_select_db('RGDindatavalid', $link);
 if (!$db_selected) {
-    die ('Can\'t use test : ' . mysql_error());
+    die ('Can\'t use RGDindatavalid : ' . mysql_error());
 }
 echo 'connected successfully to '.$db_selected."<br/>";
 ?>
