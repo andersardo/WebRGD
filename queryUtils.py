727d301a47b98e90ee3f8a0d4d22bfbb01d61942
Anders Ardo <Anders.Ardo@gmail.com>
2018-01-29 12:51:23 +0100

    Added posibility to search import utilities

---
+++ queryUtils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+
+from luceneDB import luceneDB
+from uiUtils import persDisp
+
+def doQuery(q, config):
+    searchDB = luceneDB(config['workDB'])
+    res = [['Namn, Id', 'Född', 'Död', 'score', 'action']]
+    for uid, score in searchDB.query(q):
+        pers = config['persons'].find_one({'_id': uid})
+        t = persDisp(pers)
+        t[0] += '<br/>' + uid
+        t.append("%3.0f" % (score))
+        args = {'where': 'visa', 'what': '/view/relErr', 'typ': 'queryhits',
+                'person': str(uid), 'family': ''}
+        visa = '<button onclick="doAction('+str(args)+')">Visa</button>'
+        t.append(visa)
+        res.append(t)
+    tit = u'Resultat sökning i %s efter "%s"' % (config['workDB'], q)
+    #tit = config['workDB']
+    return (tit, res)
